bccf83f59a1946e6b3605325572a232e50b6e44e
Andy Soell <andy@teamsoell.com>
2021-07-29 12:08:33 +0200

    move title and follow button to header

--- resources/views/livewire/follow-button.blade.php
+++ resources/views/livewire/follow-button.blade.php
@@ -1,7 +1,8 @@
-<div>
+<button
+    class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition mt-2 mr-2"
     @if (auth()->user()->following->contains($user))
-    <button wire:click="unfollow">Unfollow</button>
+    wire:click="unfollow">Unfollow
     @else
-    <button wire:click="follow">Follow</button>
+    wire:click="follow">Follow
     @endif
-</div>
+</button>
