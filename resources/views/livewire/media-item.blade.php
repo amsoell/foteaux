66a68ce51872962936bcbeb3c6e26e0e15bf6416
Andy Soell <andy@teamsoell.com>
2021-07-29 13:31:17 +0200

    add styling to media items

--- resources/views/livewire/media-item.blade.php
+++ resources/views/livewire/media-item.blade.php
@@ -1,4 +1,12 @@
-<div class="media-item">
-    <img src="{{ $item->url }}" />
-    <caption>{{ $item->caption }}</caption>
+<div class="flex flex-wrap media-item pb-6">
+    <img class="md:w-1/2 w-full flex-none" src="{{ $item->url }}" />
+    <div class="md:flex-1 w-full p-6">
+        <div class="meta pb-3">
+            <strong>{{ $item->user->name}}</strong>
+            <span class="text-xs ml-3">{{ $item->created_at->diffForHumans() }}</span>
+        </div>
+        <caption>
+            {{ $item->caption }}
+        </caption>
+    </div>
 </div>
