d27e32cba8fdc0390c4c7188a22d55fecadb1742
Andy Soell <andy@teamsoell.com>
2021-07-29 12:50:47 +0200

    add styling to photo upload component

--- resources/views/livewire/upload.blade.php
+++ resources/views/livewire/upload.blade.php
@@ -1,22 +1,18 @@
-<div>
-    <h1>Upload a new photo</h1>
+<div class="m-6">
     <form wire:submit.prevent="save">
-        <div>
-            <x-jet-label for="media" value="{{ __('Photo') }}" />
-            <x-jet-input id="media" class="block mt-1 w-full" type="file" name="media" wire:model="media" />
-            @error('media') <span class="error">{{ $message }}</span> @enderror
+        <div class="border-dashed border-2 border-light-blue-500 p-6">
             @if ($media)
-                Photo Preview:
                 <img src="{{ $media->temporaryUrl() }}">
             @endif
+            @error('media') <span class="error">{{ $message }}</span> @enderror
+            <x-jet-input id="media" class="block mt-1 w-full" type="file" name="media" wire:model="media" />
         </div>
-        <div>
-            <x-jet-label for="caption" value="{{ __('caption') }}" />
-            <x-jet-input id="caption" class="block mt-1 w-full" type="text" name="caption" wire:model="caption" />
+        <div class="pt-6">
+            <textarea id="caption" class="block w-full border-solid border-2 border-light-blue-500 p-6" type="text" name="caption" placeholder="Caption" wire:model="caption"></textarea>
             <x-jet-input-error for="caption" class="mt-2" />
         </div>
 
 
-        <button type="submit">Add photo</button>
+        <button type="submit" class="inline-flex items-center px-4 py-2 bg-white border border-gray-300 rounded-md font-semibold text-xs text-gray-700 uppercase tracking-widest shadow-sm hover:text-gray-500 focus:outline-none focus:border-blue-300 focus:ring focus:ring-blue-200 active:text-gray-800 active:bg-gray-50 disabled:opacity-25 transition mt-2 mr-2">Add photo</button>
     </form>
 </div>
