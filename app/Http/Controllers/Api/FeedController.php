f54a05ec3c121f3bd0d39f838f46d4f6411eb3b6
Andy Soell <andy@teamsoell.com>
2021-07-31 21:05:45 +0200

    add meta / link attributes to API documentation

--- app/Http/Controllers/Api/FeedController.php
+++ app/Http/Controllers/Api/FeedController.php
@@ -18,7 +18,22 @@ class FeedController extends Controller
      *        response=200,
      *        description="Successful media retrieval",
      *        @OA\JsonContent(
-     *           @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Media"))
+     *           @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Media")),
+     *           @OA\Property(property="links", type="array", @OA\Items(
+     *               @OA\Property(property="first", type="string", example="https://foteaux.test/api/v1/feed?page=1"),
+     *               @OA\Property(property="last", type="string", example="https://foteaux.test/api/v1/feed?page=3"),
+     *               @OA\Property(property="prev", type="string", example="https://foteaux.test/api/v1/feed?page=1"),
+     *               @OA\Property(property="next", type="string", example="https://foteaux.test/api/v1/feed?page=3")
+     *           )),
+     *           @OA\Property(property="meta", type="array", @OA\Items(
+     *               @OA\Property(property="current_page", type="number"),
+     *               @OA\Property(property="from", type="number", example="16"),
+     *               @OA\Property(property="last_page", type="number", example="3"),
+     *               @OA\Property(property="path", type="string", example="https://foteaux.test/api/v1/feed?page=2"),
+     *               @OA\Property(property="per_page", type="number", example="15"),
+     *               @OA\Property(property="to", type="number", example="30"),
+     *               @OA\Property(property="total", type="number", example="38")
+     *           ))
      *        )
      *     )
      * )
--- app/Http/Controllers/Api/UserMediaController.php
+++ app/Http/Controllers/Api/UserMediaController.php
@@ -29,7 +29,22 @@ class UserMediaController extends Controller
      *        response=200,
      *        description="Successful user media records lookup",
      *        @OA\JsonContent(
-     *           @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Media"))
+     *           @OA\Property(property="data", type="array", @OA\Items(ref="#/components/schemas/Media")),
+     *           @OA\Property(property="links", type="array", @OA\Items(
+     *               @OA\Property(property="first", type="string", example="https://foteaux.test/api/v1/users/jsmith/media?page=1"),
+     *               @OA\Property(property="last", type="string", example="https://foteaux.test/api/v1/users/jsmith/media?page=3"),
+     *               @OA\Property(property="prev", type="string", example="https://foteaux.test/api/v1/users/jsmith/media?page=1"),
+     *               @OA\Property(property="next", type="string", example="https://foteaux.test/api/v1/users/jsmith/media?page=3")
+     *           )),
+     *           @OA\Property(property="meta", type="array", @OA\Items(
+     *               @OA\Property(property="current_page", type="number"),
+     *               @OA\Property(property="from", type="number", example="16"),
+     *               @OA\Property(property="last_page", type="number", example="3"),
+     *               @OA\Property(property="path", type="string", example="https://foteaux.test/api/v1/users/jsmith/media?page=2"),
+     *               @OA\Property(property="per_page", type="number", example="15"),
+     *               @OA\Property(property="to", type="number", example="30"),
+     *               @OA\Property(property="total", type="number", example="38")
+     *           ))
      *        )
      *     ),
      *     @OA\Response(
